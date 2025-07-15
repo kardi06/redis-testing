cf848b6d895f1239758ebab4d8153ca3720abdb1
kardi.ib <kardi.ib@gmail.com>
2025-07-15 21:53:33 +0700

    fix(index): enable restaurants and cuisines routes in main application

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import express from "express";
-// import restaurantsRouter from "./routes/restaurants.js";
-// import cuisinesRouter from "./routes/cuisines.js";
+import restaurantsRouter from "./routes/restaurants.js";
+import cuisinesRouter from "./routes/cuisines.js";
 // import { errorHandler } from "./middlewares/errorHandler.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
-// app.use("/restaurants", restaurantsRouter);
-// app.use("/cuisines", cuisinesRouter);
+app.use("/restaurants", restaurantsRouter);
+app.use("/cuisines", cuisinesRouter);
 
 // app.use(errorHandler);
 app
