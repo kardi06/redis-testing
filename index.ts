5e7cee927235d0c159399fbc2edf9f7912c03bb4
kardi.ib <kardi.ib@gmail.com>
2025-07-15 21:59:43 +0700

    fix(index): enable error handling middleware in main application

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import restaurantsRouter from "./routes/restaurants.js";
 import cuisinesRouter from "./routes/cuisines.js";
-// import { errorHandler } from "./middlewares/errorHandler.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use("/restaurants", restaurantsRouter);
 app.use("/cuisines", cuisinesRouter);
 
-// app.use(errorHandler);
+app.use(errorHandler);
 app
   .listen(PORT, () => {
     console.log(`Application running on port ${PORT}`);
