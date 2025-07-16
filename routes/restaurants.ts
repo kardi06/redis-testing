194f23eab5b6c1820a13d8b567e459031065d7d5
kardi.ib <kardi.ib@gmail.com>
2025-07-16 20:44:05 +0700

    fix(routes restaurant): add validation to restaurants GET endpoint using RestaurantSchema

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import { validate } from "../middlewares/validate.js";
+import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
 
 
 const router = express.Router();
+
+router.get("/",validate(RestaurantSchema), async (req, res, next) => {
+  const data = req.body as Restaurant;
+  res.send(data);
+});
 
 export default router;
