8c8d610d51d6949d683aa6efbcdccc4de5fbd0a7
kardi.ib <kardi.ib@gmail.com>
2025-07-17 23:00:05 +0700

    feat(routes): initialize Redis client in restaurants GET endpoint

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
+import { initializeRedisClient } from "../utils/client.js";
 
 
 const router = express.Router();
 
 router.get("/",validate(RestaurantSchema), async (req, res, next) => {
   const data = req.body as Restaurant;
+  const client = await initializeRedisClient();
   res.send(data);
 });
 
