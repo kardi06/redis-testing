9c79085252659c7c6f88f52178d324bee4ced4a2
kardi.ib <kardi.ib@gmail.com>
2025-07-17 23:20:30 +0700

    feat(routes): enhance restaurants GET endpoint to add restaurant data to Redis

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -2,13 +2,27 @@ import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
 import { initializeRedisClient } from "../utils/client.js";
+import { nanoid } from "nanoid";
+import { restaurantKeyById } from "../utils/keys.js";
+import { successResponse } from "../utils/responses.js";
 
 
 const router = express.Router();
 
 router.get("/",validate(RestaurantSchema), async (req, res, next) => {
   const data = req.body as Restaurant;
-  const client = await initializeRedisClient();
+  try{
+    const client = await initializeRedisClient();
+    const id = nanoid();
+    const restaurantKey = restaurantKeyById(id);
+    const hashData = {id, name: data.name, location: data.location};
+    const addResult = await client.hSet(restaurantKey, hashData);
+    console.log(`Added ${addResult} field`);
+    return successResponse(res, hashData, "Restaurant added successfully");
+  }catch(error){
+    next(error);
+  }
+
   res.send(data);
 });
 
