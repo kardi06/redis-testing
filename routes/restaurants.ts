d0a640dc81356af6d9f0aacfceafd4d8cc650f59
kardi.ib <kardi.ib@gmail.com>
2025-07-21 21:23:17 +0700

    feat(routes): integrate restaurant existence check middleware in GET endpoint

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,37 +1,49 @@
-import express from "express";
+import express, { type Request } from "express";
 import { validate } from "../middlewares/validate.js";
 import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
 import { initializeRedisClient } from "../utils/client.js";
 import { nanoid } from "nanoid";
 import { restaurantKeyById } from "../utils/keys.js";
 import { successResponse } from "../utils/responses.js";
-
+import { checkRestaurantExists } from "../middlewares/checkRestaurantId.js";
 
 const router = express.Router();
 
-router.get("/:restaurantId", async (req, res, next) => {
-  const { restaurantId } = req.params;
-  try{
-    const client = await initializeRedisClient();
-    const restaurantKey = restaurantKeyById(restaurantId);
-    const restaurant = await client.hGetAll(restaurantKey);
-    return successResponse(res, restaurant, "Restaurant fetched successfully");
-  }catch (error){
-    next(error);
+router.get(
+  "/:restaurantId",
+  checkRestaurantExists,
+  async (
+    req: Request<{ restaurantId: string }>,
+    res,
+    next
+  ) => {
+    const { restaurantId } = req.params;
+    try {
+      const client = await initializeRedisClient();
+      const restaurantKey = restaurantKeyById(restaurantId);
+      const restaurant = await client.hGetAll(restaurantKey);
+      return successResponse(
+        res,
+        restaurant,
+        "Restaurant fetched successfully"
+      );
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.post("/",validate(RestaurantSchema), async (req, res, next) => {
+router.post("/", validate(RestaurantSchema), async (req, res, next) => {
   const data = req.body as Restaurant;
-  try{
+  try {
     const client = await initializeRedisClient();
     const id = nanoid();
     const restaurantKey = restaurantKeyById(id);
-    const hashData = {id, name: data.name, location: data.location};
+    const hashData = { id, name: data.name, location: data.location };
     const addResult = await client.hSet(restaurantKey, hashData);
     console.log(`Added ${addResult} field`);
     return successResponse(res, hashData, "Restaurant added successfully");
-  }catch(error){
+  } catch (error) {
     next(error);
   }
 
