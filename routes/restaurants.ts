e25950793504fe67f6740263ca391abc3de23199
kardi.ib <kardi.ib@gmail.com>
2025-07-24 14:00:24 +0700

    feat(routes): implement POST endpoint for adding reviews and refactor restaurant addition logic

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,14 +1,49 @@
 import express, { type Request } from "express";
 import { validate } from "../middlewares/validate.js";
-import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
+import { RestaurantSchema, type Restaurant} from "../schemas/restaurant.js";
+import { ReviewSchema, type Review } from "../schemas/review.js";
 import { initializeRedisClient } from "../utils/client.js";
 import { nanoid } from "nanoid";
-import { restaurantKeyById } from "../utils/keys.js";
+import { restaurantKeyById, reviewKeyById, reviewDetailsKeyById } from "../utils/keys.js";
 import { successResponse } from "../utils/responses.js";
 import { checkRestaurantExists } from "../middlewares/checkRestaurantId.js";
 
 const router = express.Router();
+router.post("/", validate(RestaurantSchema), async (req, res, next) => {
+  const data = req.body as Restaurant;
+  try {
+    const client = await initializeRedisClient();
+    const id = nanoid();
+    const restaurantKey = restaurantKeyById(id);
+    const hashData = { id, name: data.name, location: data.location };
+    const addResult = await client.hSet(restaurantKey, hashData);
+    console.log(`Added ${addResult} field`);
+    return successResponse(res, hashData, "Restaurant added successfully");
+  } catch (error) {
+    next(error);
+  }
 
+  res.send(data);
+});
+
+router.post("/:restaurantId/reviews", checkRestaurantExists, validate(ReviewSchema), async (req: Request<{ restaurantId: string }>, res, next) => {
+  const {restaurantId} = req.params;
+  const data = req.body as Review;
+  try{
+    const client = await initializeRedisClient();
+    const reviewId = nanoid();
+    const reviewKey = reviewKeyById(reviewId);
+    const reviewDetailsKey = reviewDetailsKeyById(reviewId);
+    const reviewData = {id: reviewId, ...data, timestamp: Date.now(), restaurantId};
+    await Promise.all([
+      client.lPush(reviewKey, reviewId),
+      client.hSet(reviewDetailsKey, reviewData),
+    ]);
+    return successResponse(res, reviewData, "Review added successfully");
+  }catch(error){
+    next(error);
+  }
+});
 router.get(
   "/:restaurantId",
   checkRestaurantExists,
@@ -32,21 +67,6 @@ router.get(
   }
 );
 
-router.post("/", validate(RestaurantSchema), async (req, res, next) => {
-  const data = req.body as Restaurant;
-  try {
-    const client = await initializeRedisClient();
-    const id = nanoid();
-    const restaurantKey = restaurantKeyById(id);
-    const hashData = { id, name: data.name, location: data.location };
-    const addResult = await client.hSet(restaurantKey, hashData);
-    console.log(`Added ${addResult} field`);
-    return successResponse(res, hashData, "Restaurant added successfully");
-  } catch (error) {
-    next(error);
-  }
 
-  res.send(data);
-});
 
 export default router;
