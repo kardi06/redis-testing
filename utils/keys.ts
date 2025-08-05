150a219f71bbbd347c076134655b102a1bcff40a
kardi.ib <kardi.ib@gmail.com>
2025-08-05 09:44:12 +0700

    fix(utils): correct key generation function for cuisines in Redis

--- utils/keys.ts
+++ utils/keys.ts
@@ -10,5 +10,5 @@ export const restaurantKeyById = (id:string) => getKeysName("restaurants",id);
 export const reviewKeyById = (id:string) => getKeysName("reviews",id);
 export const reviewDetailsKeyById = (id:string) => getKeysName("review_details",id);
 
-export const cuisinesKey = getKeyName("cuisines");
-export const cuisineKey = (name: string) => getKeyName("cuisine", name);
+export const cuisinesKey = getKeysName("cuisines");
+export const cuisineKey = (name: string) => getKeysName("cuisine", name);
